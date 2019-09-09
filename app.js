b35dc321955fbe5c704b7225a4daea09b33da732
Guruprasad Kulkarni <guru@linux.com>
2019-09-09 08:40:28 +0200

    Simple Page in progress

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 const name = 'comdotlinux';
 console.log(`My Name is ${name}`)
-
-const h1 = document.querySelector("h1");
-h1.innerHTML += 'from JS';
---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+browser-sync start --server . --files . --no-ui --no-notify --browser "google-chrome"
